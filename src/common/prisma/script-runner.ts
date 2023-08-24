af5f8ced661d2b9ddcf77395332bdf258bf71c66
Enosejolagbon Abhademere <enosefelix@gmail.com>
2023-08-24 08:13:43 +0100

    modified script-runner

--- src/common/prisma/script-runner.ts
+++ src/common/prisma/script-runner.ts
@@ -1,14 +1,19 @@
 import { exec } from 'child_process';
 import * as readline from 'readline';
+import { PrismaClient } from '@prisma/client';
+import { roleSeed } from '../prisma/role.seed';
+import { ROLE_TYPE } from '../interfaces';
+import * as moment from 'moment';
+import { AppUtilities } from '../../app.utilities';
 
 const scripts = [
   // 'db.migration',
   'yarn.install',
   // 'ts.node',
-  // 'db.seed',
   // 'node',
   'db.migration.run',
   'db.generate',
+  // 'db.seed',
   // 'build',
 ];
 
@@ -64,3 +69,49 @@ async function runScriptsSequentially() {
 }
 
 runScriptsSequentially();
+
+async function seedDatabase() {
+  const tenantPrisma = new PrismaClient();
+  const promises = [];
+
+  try {
+    // Hash the password asynchronously
+    const hashedPassword = (await AppUtilities.hasher('Admin@123')) as string;
+
+    // Seed roles
+    promises.push(
+      tenantPrisma.role.createMany({
+        data: roleSeed,
+        skipDuplicates: true,
+      }),
+    );
+
+    // Fetch the ADMIN role
+    const role = await tenantPrisma.role.findFirst({
+      where: { code: ROLE_TYPE.ADMIN },
+    });
+
+    // Create a user with the hashed password
+    promises.push(
+      tenantPrisma.user.create({
+        data: {
+          email: 'admin@mail.com',
+          password: hashedPassword,
+          roleId: role.id,
+          createdAt: moment().toISOString(),
+        },
+      }),
+    );
+
+    // Execute all promises concurrently
+    await Promise.all(promises);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  } finally {
+    // Close the Prisma client connection
+    await tenantPrisma.$disconnect();
+  }
+}
+
+// Call the async seed function
+seedDatabase();
