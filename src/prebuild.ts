920c180fc93062a14254e002bf0be50a55d39379
Enosejolagbon Abhademere <enosefelix@gmail.com>
2023-10-13 11:14:04 +0100

    script to check if .env file exist, if not create one

--- src/prebuild.ts
+++ src/prebuild.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 import * as fs from 'fs';
 import * as path from 'path';
@@ -16,7 +16,26 @@ if (fs.existsSync(envFilePath)) {
   console.log('.env file exists in the codebase.');
 } else {
   console.error('.env file does not exist in the codebase.');
+  if (fs.existsSync(envFilePath)) {
+    console.log('.env file exists in the codebase.');
+  } else {
+    console.log('.env file does not exist in the codebase. Creating one...');
+
+    // Create a sample content for the .env file
+    const envFileContent = `DATABASEURL=your_database_url_here`;
+
+    // Write the content to the .env file
+    fs.writeFileSync(envFilePath, envFileContent);
+
+    console.log('.env file created.');
+  }
 }
 
-const DATABASEURL = process.env.DATABASEURL;
-console.log('🚀 ~ file: prebuild.ts:6 ~ DATABASEURL:', DATABASEURL);
+async function loadEnvVariables() {
+  await dotenv.config();
+  const DATABASEURL = process.env.DATABASEURL;
+  console.log('🚀 ~ file: prebuild.ts:6 ~ DATABASEURL:', DATABASEURL);
+}
+
+// Call the function
+loadEnvVariables();
